f6dc7ce414c9470e995eef4cc20f05c5ceaa1786
anacfsoares <anacarol.fs@hotmail.com>
2021-08-30 15:00:38 -0300

    Exercicio JPI 6 - Imprimir

--- src/com/jpiimprimir/Main.java
+++ src/com/jpiimprimir/Main.java
@@ -1,8 +1,24 @@
 package com.jpiimprimir;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+
+        List<String> habilidades = new ArrayList<>();
+        habilidades.add("Programadora Java");
+        habilidades.add("Fluente em Espanhol");
+        habilidades.add("Programadora Front-end");
+
+        Curriculo curriculo = new Curriculo("Carol", "15", new Endereco("Rua das Rosas", 34, "37570-000"), habilidades);
+        curriculo.imprimir();
+
+        LivroPdf livro = new LivroPdf(30, "Alberto Caeiro", "O guardador de rebanhos", "Poesia");
+        livro.imprimir();
+
+        Relatorio relatorio = new Relatorio(20, 50, "José Siqueira", "Ana Luiza");
+        relatorio.imprimir();
     }
 }
